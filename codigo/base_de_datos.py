3bd7add9adf174ee1ff13134e40e63474fad5291
AgustinPalladino <agustin.palladino@correo.ucu.edu.uy>
2025-06-22 21:05:33 -0300

    login

--- codigo/base_de_datos.py
+++ codigo/base_de_datos.py
@@ -2,3 +2,17 @@ import mysql.connector
 
 def conexion():
     return mysql.connector.connect(host='127.0.0.1', user='root', password='rootpassword', database= 'cafesmarloy')
+
+def login(correo, contraseña):
+    cnx = conexion()
+    cursor = cnx.cursor()
+
+    cursor.execute("select correo, es_administrador from login where correo = %s and contraseña = %s", (correo, contraseña))
+    user = cursor.fetchone()
+
+    user.close()
+
+    if user:
+        return user
+    else:
+        return None #Usar string?
