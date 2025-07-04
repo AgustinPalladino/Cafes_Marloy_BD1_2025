d210e6e078ee44d78b2555f61468d5d12ca641ac
Seba-Fon <sebastian.fonseca@correo.ucu.edu.uy>
2025-07-03 21:44:59 -0300

    Se agregaron errores

--- codigo/base_de_datos.py
+++ codigo/base_de_datos.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 def conexion():
-    return mysql.connector.connect(host='127.0.0.1', user='root', password='Cambiar contraseña', database= 'CafesMarloy')
+    return mysql.connector.connect(host='127.0.0.1', user='root', password='07102005Sebastian', database= 'CafesMarloy')
 
 def login(correo, contraseña):
     cnx = conexion()
